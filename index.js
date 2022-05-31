8370c0e167eed839f0a5003cb3575250b122937c
Richmond Dominguez <richmonddominguez@gmail.com>
2022-05-31 00:17:31 -0700

    added node and started in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const inqirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const team = [];
